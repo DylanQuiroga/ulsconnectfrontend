836d46779aee7cb228dcc9aa4d09baf4c5300c78
BenjaSilva19 <benjamin.silva@userena.cl>
2025-11-06 15:47:40 -0300

    añadido react routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 // Importa las páginas o secciones
+import HeroSection from "./components/HeroSection";
 import RegisterSection from "./components/RegisterSection";
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
+      <Route path="/" element={<HeroSection />} />
       <Route path="/register" element={<RegisterSection />} />
     </Routes>
   );
 };
 
 export default AppRoutes;
-
