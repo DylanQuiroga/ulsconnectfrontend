545ede1f155c72464de62792fd2494c8fa6227fd
BenjaSilva19 <benjamin.silva@userena.cl>
2025-11-13 12:28:47 -0300

    prototipo panel de control de voluntario

---
+++ src/pages/VolunteerProfile.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import VolunteerSection from "../components/VolunteerSection";
+
+const VolunteerProfile: React.FC = () => {
+  return (
+    <>
+      <VolunteerSection />
+    </>
+  );
+};
+
+export default VolunteerProfile;
